43bcb1387d4bf0a6c4aec96ebea6d796890c6473
britt-daniel-rsi <britt.daniel@ruralsourcing.com>
2015-02-18 17:31:44 -0500

    Jsf update

--- EmployeeSkillsKnowledgeBase/src/main/java/com/rsi/esk/service/UserService.java
+++ EmployeeSkillsKnowledgeBase/src/main/java/com/rsi/esk/service/UserService.java
@@ -4,44 +4,43 @@ import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import com.rsi.esk.dao.UserDao;
 import com.rsi.esk.domain.User;
 
-//@Component
-@Service
+@Component
 public class UserService {
-	
-	private UserDao userDao;
-	 
-	public UserDao getUserDao() {
-	return userDao;
-	}
-	 
+
 	@Autowired
+	private UserDao userDao;
+
+	public UserDao getUserDao() {
+		return userDao;
+	}
+
 	public void setUserDao(UserDao userDao) {
-	this.userDao = userDao;
+		this.userDao = userDao;
 	}
-	 
+
 	public void addUser(User user) {
-	//if(user.getId() != null && user.getId() > 0) {
-	user.setId(userDao.getMaxId()+ 1);
-	user.setCreateDate(Calendar.getInstance().getTime());	
-	System.out.println(user.getId());
-	getUserDao().save(user);
+		// if(user.getId() != null && user.getId() > 0) {
+		user.setId(userDao.getMaxId() + 1);
+		user.setCreateDate(Calendar.getInstance().getTime());
+		System.out.println(user.getId());
+		getUserDao().save(user);
 	}
-	 
+
 	public List<User> getAllUsers() {
-	return getUserDao().list();
+		return getUserDao().list();
 	}
-	
+
 	public List<User> SurSearch(String surname) {
 		return getUserDao().SurnameSearch(surname);
-		}
-	
+	}
+
 	public List<User> IdSearch(Integer id) {
 		return getUserDao().IdSearch(id);
-		}
-	
+	}
+
 }
