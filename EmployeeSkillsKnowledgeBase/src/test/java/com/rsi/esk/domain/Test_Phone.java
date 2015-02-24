85fdf95aa0db82981155d3f33cb3d4978fd22df4
britt-daniel-rsi <britt.daniel@ruralsourcing.com>
2015-02-24 18:29:12 -0500

    Logging and Unit Test Config

---
+++ EmployeeSkillsKnowledgeBase/src/test/java/com/rsi/esk/domain/Test_Phone.java
@@ -0,0 +1,16 @@
+package com.rsi.esk.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Test_Phone {
+
+
+	@Test
+	public void testPlus() {
+		
+		assertEquals(1+1,2);
+	}
+
+}
