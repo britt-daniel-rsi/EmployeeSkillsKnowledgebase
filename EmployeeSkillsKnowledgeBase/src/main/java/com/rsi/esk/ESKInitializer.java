9af0bfd9e76ced3680d0653b64a10af5a5dcc301
britt.daniel <britt.daniel@ehi.com>
2015-06-05 09:05:14 -0400

    Primary Change for Gradle Build

---
+++ EmployeeSkillsKnowledgeBase/src/main/java/com/rsi/esk/ESKInitializer.java
@@ -0,0 +1,29 @@
+package com.rsi.esk;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ESKInitializer implements WebApplicationInitializer {
+	public void onStartup(ServletContext servletContext) throws ServletException {  
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+System.out.println("config");
+        ctx.register(ESKConfig.class);
+
+        ctx.setServletContext(servletContext);    
+        servletContext.addListener(new ContextLoaderListener(ctx));
+        servletContext.addListener(new RequestContextListener());
+        DispatcherServlet dispatcher = new DispatcherServlet(ctx);
+        
+        Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcher);  
+        dynamic.addMapping("ESK/");  
+        dynamic.setLoadOnStartup(1);  
+   }
+
+}
